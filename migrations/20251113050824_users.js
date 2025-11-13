80135953601dfbed12133aaf952a9b28fc0c79e9
uzbdev7 <mengilovahrorbek5@gmail.com>
2025-11-13 10:27:05 +0500

    paketlar qoshildi.

---
+++ migrations/20251113050824_users.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// eslint-disable-next-line no-undef
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('email', 255).notNullable().unique();
+    table.string('username', 100).notNullable().unique();
+    table.string('password', 255).notNullable();
+    table.enu('role', ['user', 'admin', 'manager']).defaultTo('user'); 
+    table.enu('status', ['active', 'inactive']).defaultTo('active');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
